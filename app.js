fc3f5b08e8180e0a97445f39dcca8edfc66ce3f2
Riley Green <rgre2543@uni.sydney.edu.au>
2017-01-02 22:25:12 +1100

    Update app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@
 var restify = require('restify');
 var builder = require('botbuilder');
 
-var appId = process.env.MY_APP_ID || "Missing your app ID";
-var appSecret = process.env.MY_APP_SECRET || "Missing your app secret";
+var appId = process.env.MY_APP_ID || "8cf308bd-8e0c-4ed0-abea-87d5b324f08f";
+var appSecret = process.env.MY_APP_SECRET || "iQey2xXaHX270TTwgnCWFkn";
 
 // Create bot and add dialogs
 var bot = new builder.BotConnectorBot
