dd6cfda22f20ba7ad7a99a1b1ec768407acda1c5
Riley Green <rileygreen022@hotmail.com>
2017-01-02 22:47:47 +1100

    th

--- app.js
+++ app.js
@@ -1,25 +1,21 @@
-//Add your requirements
-var restify = require('restify');
-var builder = require('botbuilder');
-
-var appId = process.env.MY_APP_ID || "8cf308bd-8e0c-4ed0-abea-87d5b324f08f";
-var appSecret = process.env.MY_APP_SECRET || "iQey2xXaHX270TTwgnCWFkn";
-
-// Create bot and add dialogs
-var bot = new builder.BotConnectorBot
-({appId: process.env.MY_APP_ID, appSecret: process.env.MY_APP_SECRET});
-bot.add('/', new builder.SimpleDialog( function (session) {
-session.send('Hello World');
-}));
+// Add your requirements
+var restify = require('restify'); 
+var builder = require('botbuilder'); 
 
 // Setup Restify Server
 var server = restify.createServer();
-server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
-server.listen(process.env.port || 3000, function () {
-console.log('%s listening to %s', server.name, server.url);
+server.listen(process.env.PORT || 3000, function() 
+{
+   console.log('%s listening to %s', server.name, server.url); 
 });
 
-server.get('/', restify.serveStatic({
-    directory: __dirname,
-    default: '/index.html'
-}));
+// Create chat bot
+var connector = new builder.ChatConnector
+({ appId: 'YourAppId', appPassword: 'YourAppPassword' }); 
+var bot = new builder.UniversalBot(connector);
+server.post('/api/messages', connector.listen());
+
+// Create bot dialogs
+bot.dialog('/', function (session) {
+    session.send("Hello World");
+});
