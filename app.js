e945fd7175bd7ebfaf9b274e7938f0ae839adb17
Riley Green <rileygreen022@hotmail.com>
2017-01-02 23:07:42 +1100

    fffd
    dd

--- app.js
+++ app.js
@@ -1,21 +1,28 @@
-// Add your requirements
-var restify = require('restify'); 
-var builder = require('botbuilder'); 
+var restify = require('restify');
+var builder = require('botbuilder');
+
+//=========================================================
+// Bot Setup
+//=========================================================
 
 // Setup Restify Server
 var server = restify.createServer();
-server.listen(process.env.PORT || 3000, function() 
-{
+server.listen(process.env.port || process.env.PORT || 3978, function () {
    console.log('%s listening to %s', server.name, server.url); 
 });
 
 // Create chat bot
-var connector = new builder.ChatConnector
-({ appId: '8cf308bd-8e0c-4ed0-abea-87d5b324f08f', appPassword: 'iQey2xXaHX270TTwgnCWFkn' }); 
+var connector = new builder.ChatConnector({
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
+});
 var bot = new builder.UniversalBot(connector);
 server.post('/api/messages', connector.listen());
 
-// Create bot dialogs
+//=========================================================
+// Bots Dialogs
+//=========================================================
+
 bot.dialog('/', function (session) {
     session.send("Hello World");
 });
